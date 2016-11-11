902c316aab45436992c707571e26d0fd1d3df4d9
abdou <aa.test20@gmail.com>
2016-11-11 01:53:54 +0100

    simple translation mockup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import Layout from './Layout';
 import Counter from './Counter';
+import LocaleSwitcher  from './LocaleSwitcher';
+import Greeter  from './Greeter';
+
+//setting language support
+import counterpart  from 'counterpart';
+import Translate    from 'react-translate-component';
+counterpart.registerTranslations('en', {
+  example: {
+    greeting: 'Hello %(name)s! How are you today?'
+  }
+});
+
+counterpart.registerTranslations('de', {
+  example: {
+    greeting: 'Hallo, %(name)s! Wie geht\'s dir heute so?'
+  }
+});
 
 // If you use React Router, make this component
 // render <Router> with your routes. Currently,
@@ -13,6 +30,8 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
+      	 <LocaleSwitcher />
+      	 <Greeter with={{ name: "Martin" }} component="h1" />
         <Counter />
       </Layout>
     );
